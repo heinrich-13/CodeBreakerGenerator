271699044491dfa216d8d8be503c3bc908187ec0
Heinrich Stoltz <heinrichstoltz@macbook-air-2.local>
2019-01-15 02:35:46 +0800

    Initial Commit

---
+++ Code Breaker Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Code Breaker Generator
+//
+//  Created by Heinrich Stoltz on 2019/1/15.
+//  Copyright © 2019 Heinrich Stoltz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Code Breaker Generator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Code Breaker Generator
+//
+//  Created by Heinrich Stoltz on 2019/1/15.
+//  Copyright © 2019 Heinrich Stoltz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
